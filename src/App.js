837a5ec32286250415808b3075676b64a827e30b
roodhouse <rughjm@gmail.com>
2023-07-23 12:43:29 -0500

    validation done

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
           <div id='headerWrapper' className='mb-6 md:mb-[52px]'>
             <Header />
           </div>
-          <div id='inputWrapper' className='mb-6 md:mb-[43px]'>
+          <div id='inputWrapper' className='mb-6 md:mb-[21px]'>
             <Input />
           </div>
           <div id='wordWrapper' className='mb-[29px] md:mb-10'>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import Search from '../assets/images/icon-search.svg'
+import { useForm } from 'react-hook-form'
 
 function Input() {
+
+  const { register, resetField, handleSubmit, formState: {errors} } = useForm({defaultValues: {
+    word: ''
+}})
+
+function onSubmit() {
+  console.log('bam')
+}
+
+function onError() {
+  console.log('error')
+  document.getElementById('errorDiv').firstChild.style.display = 'block'
+  document.getElementById('inputContainer').style.borderColor = '#FF5252'
+}
+
   return (
     <>
-      <div id="inputContainer" className='px-6 py-[14px] rounded-2xl md:py-5'>
-        <form className='flex items-center justify-between'>
+      <div id="inputContainer" className='px-6 py-[14px] rounded-2xl border border-transparent md:py-5 mb-2'>
+        <form noValidate onSubmit={handleSubmit(onSubmit, onError)} className='flex items-center justify-between'>
           <div id="termContainer">
-            <input type="text" placeholder='keyboard' className='text-base font-bold placeholder:text-base placeholder:font-bold md:text-xl md:placeholder:text-xl' />
+            <input 
+              type="text" 
+              {...register('word', {
+                required: 'Whoops, can’t be empty…'
+              })}
+              id='word' 
+              name='word' 
+              placeholder='keyboard' 
+              className='text-base font-bold placeholder:text-base placeholder:font-bold md:text-xl md:placeholder:text-xl' />
           </div>
           <div id="submitContainer" className='flex items-center'>
             <button type='submit'>
@@ -15,6 +39,9 @@ function Input() {
             </button>
           </div>
         </form>
+      </div>
+      <div id="errorDiv" className='h-6'>
+        <p className='hidden text-orange'>{errors.word?.message}</p>
       </div>
     </>
   )
